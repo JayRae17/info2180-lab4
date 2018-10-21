1afeeddd86dec8e8026ced9660e803c961e83364
Jada Brown <jadarae.brown@mymona.uwi.edu>
2018-10-21 19:38:07 +0000

    3rd

--- maze.js
+++ maze.js
@@ -1,26 +1,43 @@
+var touch = 0;
 function setEvents(){
+    var e = document.querySelector('#end');
     var b = document.querySelectorAll('.boundary');
+    var touch = 0;
     
     for (var i = 0; i < b.length; i++){
-        
+      
          b[i].onmouseover = function(){
-             
-             for(var x = 0; x < b.length; x++){
-                 var elem = b[x];
-                 elem.classList.add('youlose');
-             }
-         }
-    
-    }
-
+            touch = 1;
+            
+            for(var x = 0; x < b.length; x++){
+                var elem = b[x];
+                elem.classList.add('youlose');
+            }
+            
+            if (touch == 1){
+                 e.onmouseover= function(){
+                    alert("You Lost");
+                 };
+            }
+        }; 
+     e.onmouseover= function(){
+        alert("You Won!");
+     };
+            
+                
+    } 
 }
-
-
-
+   // var check = function(e) {
+	  //  if(e.type == 'mouseover') {
+	//	console.log(10);
+    //	}
+//};
+    
+  // check(b);
+    
 window.onload= function(){
     setEvents();
 };
 
 
 
-    
